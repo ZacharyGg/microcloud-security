2b10f180fac9efdd5fe84e6d17e0483a16a0cd29
ZacharyGg <zhang6269@163.com>
2018-07-11 20:59:45 +0800

    ssdf

---
+++ microcloud-security-client/src/test/java/cn/hlxd/security/controller/UserControllerTest.java
@@ -0,0 +1,46 @@
+package cn.hlxd.security.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author Administrator
+ * @Date 2018/7/11 19:38
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    public WebApplicationContext webApplicationContext;
+
+    public MockMvc mockMvc;
+
+    @Before
+    public void stepUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void whenQuerySuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user")
+                .param("username","jojo")
+                .param("age","18")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));
+    }
+}
