307d25b6a6f02e9e47156ecb10591c91bc5c5b20
ZacharyGg <zhang6269@163.com>
2018-07-12 19:07:46 +0800

    ces

---
+++ microcloud-security-browser/src/main/java/cn/hlxd/security/App.java
@@ -0,0 +1,15 @@
+package cn.hlxd.security;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
